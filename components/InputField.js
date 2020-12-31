0f7d170622157cd74b7d25f0e1784fdb47d9d64c
Kevin Luo <kvn.luo@gmail.com>
2020-12-31 16:22:48 -0500

    inputfield component and login page with formik

--- components/InputField.js
+++ components/InputField.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import {
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  FormControl,
+  Textarea,
+} from "@chakra-ui/react";
+import { useField } from "formik";
 
-export default function InputField() {
+export default function InputField({ label, textarea, ...props }) {
+  let InputOrTextarea;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  } else {
+    InputOrTextarea = Input;
+  }
+
+  const [field, { error }] = useField(props);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <InputOrTextarea {...field} {...props} id={field.name} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
 }
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Input, Box } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useMutation } from "@apollo/client";
+import { Formik, Form } from "formik";
+
+import { withApollo } from "../utils/withApollo";
+import { LOGIN_MUTATION } from "../graphql/mutations/login";
 
 function Login() {
+  const router = useRouter();
+  const [login] = useMutation(LOGIN_MUTATION);
+  const initialValues = { nameOrEmail: "", password: "" };
+
+  const handleSubmit = async (values, setErrors) => {
+    const response = await login({ variables: values });
+    if(response.data?.login.errors) {
+      setErrors(toErrorMap(response.data.login.errors));
+    } else if (response.data?.login.user) {
+      router.push("/");
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Box>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values, { setErrors }) =>
+          await handleSubmit(values, setErrors)
+        }
+      ></Formik>
+    </Box>
+  );
 }
 
-export default Login;
+export default withApollo({ ssr: true })(Login);
